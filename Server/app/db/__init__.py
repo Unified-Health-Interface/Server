352e9974f63f6887e9e560ecec814930a2bb44e1
Harikeshav R <r.harikeshav@gmail.com>
2023-01-29 13:15:24 +0530

    Create get_db generator

--- Server/app/db/__init__.py
+++ Server/app/db/__init__.py
@@ -1,1 +1,1 @@
-from app.db.db import Base, SessionLocal
+from app.db.db import Base, SessionLocal, get_db
--- Server/app/db/db.py
+++ Server/app/db/db.py
@@ -8,3 +8,13 @@ engine = create_engine(settings.SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(auto_commit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+
+    finally:
+        db.close()
