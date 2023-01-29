ddf7303ed0dc6b808491f15fdd62e716e33c83b4
Harikeshav R <r.harikeshav@gmail.com>
2023-01-29 16:24:00 +0530

    Create all database tables

--- Server/app/main.py
+++ Server/app/main.py
@@ -1,6 +1,10 @@
 from app.api import api_router
 from app.core import settings
 from fastapi import FastAPI
+from app.db import Base, engine
+
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title=settings.PROJECT_NAME)
 app.include_router(api_router)
