7427dde840d603db4bf186c86d42c88b6905f80f
Harikeshav R <r.harikeshav@gmail.com>
2023-01-29 12:45:17 +0530

    Create database interface

---
+++ Server/app/db/db.py
@@ -0,0 +1,10 @@
+from app.core import settings
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine(settings.SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(auto_commit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
