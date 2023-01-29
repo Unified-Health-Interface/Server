f791a43bbe6ff76b93ab5dedff4ff0fd74bbaa81
Harikeshav R <r.harikeshav@gmail.com>
2023-01-29 15:05:10 +0530

    Load .env file

--- Server/app/core/config.py
+++ Server/app/core/config.py
@@ -3,9 +3,12 @@ import os
 from dotenv import load_dotenv
 
 
+load_dotenv()
+
+
 class Settings:
     PROJECT_NAME = "UHI"
-    DATABASE_USERNAME = os.environ.get("DATABASE_USER")
+    DATABASE_USERNAME = os.environ.get("DATABASE_USERNAME")
     DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
     SQLALCHEMY_DATABASE_URL = (
         f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@localhost/UHI"
