7734fee468a830b347e1ed06180fcede96292231
Harikeshav R <r.harikeshav@gmail.com>
2023-01-29 12:45:01 +0530

    Add database fields to settings

--- Server/app/core/config.py
+++ Server/app/core/config.py
@@ -1,5 +1,15 @@
+import os
+
+from dotenv import load_dotenv
+
+
 class Settings:
     PROJECT_NAME = "UHI"
+    DATABASE_USERNAME = os.environ.get("DATABASE_USER")
+    DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
+    SQLALCHEMY_DATABASE_URL = (
+        f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@localhost/UHI"
+    )
 
 
 settings = Settings()
