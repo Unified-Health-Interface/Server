c2fadbc947357b6adc34ba4e597905ef994b96e8
Harikeshav R <r.harikeshav@gmail.com>
2023-01-29 16:23:41 +0530

    Create user create route

--- Server/app/api/__init__.py
+++ Server/app/api/__init__.py
@@ -1,3 +1,6 @@
+from app.api.endpoints import user
+from app.db import get_db
 from fastapi import APIRouter
 
 api_router = APIRouter()
+api_router.include_router(user.router, prefix="/user")
---
+++ Server/app/api/endpoints/user.py
@@ -0,0 +1,13 @@
+from app.db import get_db
+from app.schemas import UserCreate, User
+from app.crud import create_user
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+router = APIRouter()
+
+
+@router.post("/", response_model=User)
+async def create(*, db: Session = Depends(get_db), user: UserCreate):
+    db_user = create_user(db, user=user)
+    return db_user
