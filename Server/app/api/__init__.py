ad31321b6200a491c10f8e10813647bd7812be10
Harikeshav R <r.harikeshav@gmail.com>
2023-01-30 11:48:11 +0530

    Import vaccination route to api/__init__.py

--- Server/app/api/__init__.py
+++ Server/app/api/__init__.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import allergy, user
+from app.api.endpoints import allergy, user, vaccination
 
 api_router = APIRouter()
 api_router.include_router(user.router, prefix="/user")
 api_router.include_router(allergy.router, prefix="/allergy")
+api_router.include_router(vaccination.router, prefix="/vaccination")
