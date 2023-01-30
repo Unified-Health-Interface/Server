5a155ad3e73eff92d138767d50fb7e0c3423bcd0
Harikeshav R <r.harikeshav@gmail.com>
2023-01-30 13:20:03 +0530

    Import appointment schemas to schemas/__init__.py

--- Server/app/schemas/__init__.py
+++ Server/app/schemas/__init__.py
@@ -2,3 +2,4 @@ from app.schemas.allergy import Allergy, AllergyBase, AllergyCreate, AllergyInDB
 from app.schemas.basic_health import BasicHealth, BasicHealthBase, BasicHealthInDBBase, BasicHealthCreate
 from app.schemas.user import User, UserBase, UserCreate, UserInDBBase
 from app.schemas.vaccination import Vaccination, VaccinationBase, VaccinationInDBBase, VaccinationCreate
+from app.schemas.appointment import Appointment, AppointmentBase, AppointmentInDBBase, AppointmentCreate
