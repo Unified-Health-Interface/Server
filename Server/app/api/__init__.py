1520d855d87346897c5bf1a7e194ef3d604c9556
Harikeshav R <r.harikeshav@gmail.com>
2023-01-30 15:50:58 +0530

    Import prescription api router to api/__init__.py

--- Server/app/api/__init__.py
+++ Server/app/api/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import allergy, user, vaccination, basic_health, appointment
+from app.api.endpoints import allergy, user, vaccination, basic_health, appointment, prescription
 
 api_router = APIRouter()
 api_router.include_router(user.router, prefix="/user")
@@ -8,3 +8,4 @@ api_router.include_router(allergy.router, prefix="/allergy")
 api_router.include_router(vaccination.router, prefix="/vaccination")
 api_router.include_router(basic_health.router, prefix="/basic-health")
 api_router.include_router(appointment.router, prefix="/appointment")
+api_router.include_router(prescription.router, prefix="/prescription")
