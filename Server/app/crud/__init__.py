05eb10813174574678404b41f4e4e69ddc2d818a
Harikeshav R <r.harikeshav@gmail.com>
2023-01-30 20:15:08 +0530

    Import create_bill, read_user_bill to crud/__init__.py

--- Server/app/crud/__init__.py
+++ Server/app/crud/__init__.py
@@ -4,3 +4,4 @@ from app.crud.basic_health import create_basic_health, read_user_basic_health
 from app.crud.prescription import create_prescription, read_user_prescription
 from app.crud.user import create_user, get_user_by_username
 from app.crud.vaccination import create_vaccination, read_user_vaccinations
+from app.crud.bill import create_bill, read_user_bill
