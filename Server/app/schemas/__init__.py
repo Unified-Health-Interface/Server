6ad2d2766a36e36e3eb391a396d5f33ceffbcb5d
Harikeshav R <r.harikeshav@gmail.com>
2023-01-30 15:39:22 +0530

    Import prescription to schemas/__init__.py

--- Server/app/schemas/__init__.py
+++ Server/app/schemas/__init__.py
@@ -1,5 +1,6 @@
 from app.schemas.allergy import Allergy, AllergyBase, AllergyCreate, AllergyInDBBase
 from app.schemas.appointment import Appointment, AppointmentBase, AppointmentInDBBase, AppointmentCreate
 from app.schemas.basic_health import BasicHealth, BasicHealthBase, BasicHealthInDBBase, BasicHealthCreate
+from app.schemas.prescription import Prescription, PrescriptionBase, PrescriptionInDBBase, PrescriptionCreate
 from app.schemas.user import User, UserBase, UserCreate, UserInDBBase
 from app.schemas.vaccination import Vaccination, VaccinationBase, VaccinationInDBBase, VaccinationCreate
