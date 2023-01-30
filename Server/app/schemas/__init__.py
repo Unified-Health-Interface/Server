616f5847d7de14d781ed42903643ddd2b0189278
Harikeshav R <r.harikeshav@gmail.com>
2023-01-30 11:48:43 +0530

    Reformat and optimise imports

--- Server/app/schemas/__init__.py
+++ Server/app/schemas/__init__.py
@@ -1,3 +1,3 @@
 from app.schemas.allergy import Allergy, AllergyBase, AllergyCreate, AllergyInDBBase
-from app.schemas.vaccination import Vaccination, VaccinationBase, VaccinationInDBBase, VaccinationCreate
 from app.schemas.user import User, UserBase, UserCreate, UserInDBBase
+from app.schemas.vaccination import Vaccination, VaccinationBase, VaccinationInDBBase, VaccinationCreate
