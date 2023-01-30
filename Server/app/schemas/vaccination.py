f8fa13dc3bfaca3f21597eaddd584c87e8bda223
Harikeshav R <r.harikeshav@gmail.com>
2023-01-30 12:10:48 +0530

    Add done field to vaccination schema

--- Server/app/schemas/vaccination.py
+++ Server/app/schemas/vaccination.py
@@ -6,12 +6,14 @@ from pydantic import BaseModel
 class VaccinationBase(BaseModel):
     vaccine: str | None = None
     date: datetime.date | None = None
+    done: bool | None = None
 
 
 class VaccinationCreate(VaccinationBase):
     vaccine: str
     date: datetime.date
     username: str
+    done: bool
 
 
 class VaccinationInDBBase(VaccinationBase):
