22de96751722c2541164e0d12535de155ed039a5
Harikeshav R <r.harikeshav@gmail.com>
2023-01-30 12:45:38 +0530

    Import basic health schemas to schemas/__init__.py

--- Server/app/schemas/__init__.py
+++ Server/app/schemas/__init__.py
@@ -1,3 +1,4 @@
 from app.schemas.allergy import Allergy, AllergyBase, AllergyCreate, AllergyInDBBase
+from app.schemas.basic_health import BasicHealth, BasicHealthBase, BasicHealthInDBBase, BasicHealthCreate
 from app.schemas.user import User, UserBase, UserCreate, UserInDBBase
 from app.schemas.vaccination import Vaccination, VaccinationBase, VaccinationInDBBase, VaccinationCreate
