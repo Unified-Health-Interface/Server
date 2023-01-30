d5b90bd050689d8ffe1f832d3659c167fadab9bc
Harikeshav R <r.harikeshav@gmail.com>
2023-01-30 13:22:10 +0530

    Import appointment to crud/__init__.py

--- Server/app/crud/__init__.py
+++ Server/app/crud/__init__.py
@@ -1,4 +1,5 @@
 from app.crud.allergy import create_allergy, read_user_allergies
+from app.crud.appointment import create_appointment, read_user_appointments
 from app.crud.basic_health import create_basic_health, read_user_basic_health
 from app.crud.user import create_user, get_user_by_username
 from app.crud.vaccination import create_vaccination, read_user_vaccinations
