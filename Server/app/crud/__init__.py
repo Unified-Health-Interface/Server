545e1ad536c6dd875c3853a85ea35d260ae59d8e
Harikeshav R <r.harikeshav@gmail.com>
2023-01-30 15:50:29 +0530

    Import crud operations to crud/__init__.py

--- Server/app/crud/__init__.py
+++ Server/app/crud/__init__.py
@@ -1,5 +1,6 @@
 from app.crud.allergy import create_allergy, read_user_allergies
 from app.crud.appointment import create_appointment, read_user_appointments
 from app.crud.basic_health import create_basic_health, read_user_basic_health
+from app.crud.prescription import create_prescription, read_user_prescription
 from app.crud.user import create_user, get_user_by_username
 from app.crud.vaccination import create_vaccination, read_user_vaccinations
