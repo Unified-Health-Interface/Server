259bea9d59cadfd9b0d8ec044828807219236c99
Harikeshav R <r.harikeshav@gmail.com>
2023-01-30 15:38:54 +0530

    Import prescription to models/__init__.py

--- Server/app/models/__init__.py
+++ Server/app/models/__init__.py
@@ -1,5 +1,6 @@
 from app.models.allergy import Allergy
 from app.models.appointment import Appointment
 from app.models.basic_health import BasicHealth
+from app.models.prescription import Prescription
 from app.models.user import User
 from app.models.vaccination import Vaccination
