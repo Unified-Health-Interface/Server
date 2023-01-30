ab25f32be7dbb7eeb5da859f34b2c34b86464676
Harikeshav R <r.harikeshav@gmail.com>
2023-01-30 13:13:13 +0530

    Create appointment model

---
+++ Server/app/models/appointment.py
@@ -0,0 +1,16 @@
+import datetime
+
+from sqlalchemy import ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db import Base
+
+
+class Appointments(Base):
+    __tablename__ = "appointments"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+    doctor: Mapped[str] = mapped_column(String)
+    hospital: Mapped[str] = mapped_column(String)
+    date_time: Mapped[datetime.datetime] = mapped_column(DateTime)
+    username: Mapped[str] = mapped_column(ForeignKey("users.username"), unique=True)
