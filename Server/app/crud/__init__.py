6e5ced4ee69862fc335333007966d52300e68cb4
Harikeshav R <r.harikeshav@gmail.com>
2023-01-30 11:41:56 +0530

    Import create_vaccination, read_user_vaccinations into crud/__init__.py

--- Server/app/crud/__init__.py
+++ Server/app/crud/__init__.py
@@ -1,2 +1,3 @@
 from app.crud.allergy import create_allergy, read_user_allergies
 from app.crud.user import create_user, get_user_by_username
+from app.crud.vaccination import create_vaccination, read_user_vaccinations
