e576c40baf28a0dcc73b3c9d5c2c6fd824592e5e
Harikeshav R <r.harikeshav@gmail.com>
2023-01-30 12:10:44 +0530

    Add done field to vaccination model

--- Server/app/models/vaccination.py
+++ Server/app/models/vaccination.py
@@ -1,6 +1,6 @@
 import datetime
 
-from sqlalchemy import ForeignKey, Integer, String, Date
+from sqlalchemy import ForeignKey, Integer, String, Date, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.db import Base
@@ -12,4 +12,5 @@ class Vaccination(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
     vaccine: Mapped[str] = mapped_column(String)
     date: Mapped[datetime.date] = mapped_column(Date)
+    done: Mapped[bool] = mapped_column(Boolean)
     username: Mapped[str] = mapped_column(ForeignKey("users.username"))
