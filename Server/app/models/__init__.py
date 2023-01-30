595e499b2031a1f880a790ad59f64d6f24c536da
Harikeshav R <r.harikeshav@gmail.com>
2023-01-30 13:19:09 +0530

    Import appointment into models/__init__.py

--- Server/app/models/__init__.py
+++ Server/app/models/__init__.py
@@ -2,3 +2,4 @@ from app.models.allergy import Allergy
 from app.models.basic_health import BasicHealth
 from app.models.user import User
 from app.models.vaccination import Vaccination
+from app.models.appointment import Appointment
