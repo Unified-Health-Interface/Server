f47991bf64b07c8b9f652cc33696bd20a54dc2ec
Harikeshav R <r.harikeshav@gmail.com>
2023-01-30 12:11:31 +0530

    Modify crud operation to check if user wants only pending vaccinations

--- Server/app/crud/vaccination.py
+++ Server/app/crud/vaccination.py
@@ -5,12 +5,16 @@ from app import models, schemas
 
 def create_vaccination(db: Session, vaccination: schemas.VaccinationCreate):
     db_vaccination = models.Vaccination(username=vaccination.username, vaccine=vaccination.vaccine,
-                                        date=vaccination.date)
+                                        date=vaccination.date, done=vaccination.done)
     db.add(db_vaccination)
     db.commit()
     db.refresh(db_vaccination)
     return db_vaccination
 
 
-def read_user_vaccinations(db: Session, username: str):
+def read_user_vaccinations(db: Session, username: str, only_pending: bool):
+    if only_pending:
+        return db.query(models.Vaccination).filter(models.Vaccination.username == username).filter(
+            models.Vaccination.done == False).all()
+
     return db.query(models.Vaccination).filter(models.Vaccination.username == username).all()
