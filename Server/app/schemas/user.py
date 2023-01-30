12eb338c484b40a3996e1f82ce51379592164d3e
Harikeshav R <r.harikeshav@gmail.com>
2023-01-30 13:17:44 +0530

    Add appointments field to user schema

--- Server/app/schemas/user.py
+++ Server/app/schemas/user.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from app.schemas.allergy import Allergy
 from app.schemas.basic_health import BasicHealth
 from app.schemas.vaccination import Vaccination
+from app.schemas.appointment import Appointment
 
 
 class UserBase(BaseModel):
@@ -12,6 +13,7 @@ class UserBase(BaseModel):
     allergies: list[Allergy] | None = None
     vaccinations: list[Vaccination] | None = None
     basic_health: BasicHealth | None = None
+    appointments: Appointment | None = None
 
 
 class UserCreate(UserBase):
