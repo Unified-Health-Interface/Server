0a40b7d9e5116a3ba7f5c82882d224d09d6e9aa6
Harikeshav R <r.harikeshav@gmail.com>
2023-01-30 11:35:03 +0530

    Import vaccination to schemas/__init__.py

--- Server/app/schemas/__init__.py
+++ Server/app/schemas/__init__.py
@@ -1,2 +1,3 @@
 from app.schemas.allergy import Allergy, AllergyBase, AllergyCreate, AllergyInDBBase
-from app.schemas.user import User, UserBase, UserCreate, UserIn, UserInDBBase
+from app.schemas.vaccination import Vaccination, VaccinationBase, VaccinationInDBBase, VaccinationCreate
+from app.schemas.user import User, UserBase, UserCreate, UserInDBBase
