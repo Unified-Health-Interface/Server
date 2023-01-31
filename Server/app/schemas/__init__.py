41b714b66b0affca85d12fb314301e8f10dacd63
Harikeshav R <r.harikeshav@gmail.com>
2023-01-31 08:43:14 +0530

    Import emergency contact schema to schemas/__init__.py

--- Server/app/schemas/__init__.py
+++ Server/app/schemas/__init__.py
@@ -2,6 +2,7 @@ from app.schemas.allergy import Allergy, AllergyBase, AllergyCreate, AllergyInDB
 from app.schemas.appointment import Appointment, AppointmentBase, AppointmentInDBBase, AppointmentCreate
 from app.schemas.basic_health import BasicHealth, BasicHealthBase, BasicHealthInDBBase, BasicHealthCreate
 from app.schemas.bill import Bill, BillBase, BillCreate, BillInDBBase
+from app.schemas.emergency_contact import EmergencyContact, EmergencyContactBase, EmergencyContactInDBBase, EmergencyContactCreate
 from app.schemas.prescription import Prescription, PrescriptionBase, PrescriptionInDBBase, PrescriptionCreate
 from app.schemas.user import User, UserBase, UserCreate, UserInDBBase
 from app.schemas.vaccination import Vaccination, VaccinationBase, VaccinationInDBBase, VaccinationCreate
