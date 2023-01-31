494439823c415d624a86dcbdb35a2c8e44a42e17
Harikeshav R <r.harikeshav@gmail.com>
2023-01-31 08:16:23 +0530

    Add tags to each router

--- Server/app/api/__init__.py
+++ Server/app/api/__init__.py
@@ -3,10 +3,10 @@ from fastapi import APIRouter
 from app.api.endpoints import allergy, user, vaccination, basic_health, appointment, prescription, bill
 
 api_router = APIRouter()
-api_router.include_router(user.router, prefix="/user")
-api_router.include_router(allergy.router, prefix="/allergy")
-api_router.include_router(vaccination.router, prefix="/vaccination")
-api_router.include_router(basic_health.router, prefix="/basic-health")
-api_router.include_router(appointment.router, prefix="/appointment")
-api_router.include_router(prescription.router, prefix="/prescription")
-api_router.include_router(bill.router, prefix="/bill")
+api_router.include_router(allergy.router, prefix="/allergy", tags=["allergy"])
+api_router.include_router(appointment.router, prefix="/appointment", tags=["appointment"])
+api_router.include_router(basic_health.router, prefix="/basic-health", tags=["basic-health"])
+api_router.include_router(bill.router, prefix="/bill", tags=["bill"])
+api_router.include_router(prescription.router, prefix="/prescription", tags=["prescription"])
+api_router.include_router(user.router, prefix="/user", tags=["user"])
+api_router.include_router(vaccination.router, prefix="/vaccination", tags=["vaccination"])
