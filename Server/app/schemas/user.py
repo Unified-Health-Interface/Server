435984903af11c894bab6b752bcd1befea5f24a8
Harikeshav R <r.harikeshav@gmail.com>
2023-01-31 10:27:26 +0530

    Add doctor note field to user schema

--- Server/app/schemas/user.py
+++ Server/app/schemas/user.py
@@ -4,6 +4,7 @@ from app.schemas.allergy import Allergy
 from app.schemas.appointment import Appointment
 from app.schemas.basic_health import BasicHealth
 from app.schemas.bill import Bill
+from app.schemas.doctor_note import DoctorNote
 from app.schemas.emergency_contact import EmergencyContact
 from app.schemas.prescription import Prescription
 from app.schemas.vaccination import Vaccination
@@ -17,6 +18,7 @@ class UserBase(BaseModel):
     appointments: list[Appointment] | None = None
     basic_health: BasicHealth | None = None
     bills: list[Bill] | None = None
+    doctor_note: list[DoctorNote] | None = None
     emergency_contacts: list[EmergencyContact] | None = None
     prescriptions: list[Prescription] | None = None
     vaccinations: list[Vaccination] | None = None
