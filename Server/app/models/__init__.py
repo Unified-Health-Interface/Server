c39cc5b1c0581c0e2b12bb6f3472c71e9f6267f8
Harikeshav R <r.harikeshav@gmail.com>
2023-01-31 10:24:01 +0530

    Import doctor note model to models/__init__.py

--- Server/app/models/__init__.py
+++ Server/app/models/__init__.py
@@ -2,6 +2,7 @@ from app.models.allergy import Allergy
 from app.models.appointment import Appointment
 from app.models.basic_health import BasicHealth
 from app.models.bill import Bill
+from app.models.doctor_note import DoctorNote
 from app.models.emergency_contact import EmergencyContact
 from app.models.prescription import Prescription
 from app.models.user import User
