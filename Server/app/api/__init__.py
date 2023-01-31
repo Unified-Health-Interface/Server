a9acea9f523467e08aaf6a3622389477a265a710
Harikeshav R <r.harikeshav@gmail.com>
2023-01-31 10:31:52 +0530

    Import doctor note route to api/__init__.py

--- Server/app/api/__init__.py
+++ Server/app/api/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import allergy, appointment, user, basic_health, bill, emergency_contact, prescription, \
+from app.api.endpoints import allergy, appointment, user, basic_health, bill, doctor_note,emergency_contact, prescription, \
     vaccination
 
 api_router = APIRouter()
@@ -8,6 +8,7 @@ api_router.include_router(allergy.router, prefix="/allergy", tags=["allergy"])
 api_router.include_router(appointment.router, prefix="/appointment", tags=["appointment"])
 api_router.include_router(basic_health.router, prefix="/basic-health", tags=["basic-health"])
 api_router.include_router(bill.router, prefix="/bill", tags=["bill"])
+api_router.include_router(doctor_note.router, prefix="/doctor-note", tags=["doctor-note"])
 api_router.include_router(emergency_contact.router, prefix="/emergency-contact", tags=["emergency-contact"])
 api_router.include_router(prescription.router, prefix="/prescription", tags=["prescription"])
 api_router.include_router(user.router, prefix="/user", tags=["user"])
