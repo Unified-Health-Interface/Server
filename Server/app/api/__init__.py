0b12fca19fb07518d378460ef8260008d3bf0c7f
Harikeshav R <r.harikeshav@gmail.com>
2023-02-01 14:20:49 +0530

    Add missing / to notify runner prefix

--- Server/app/api/__init__.py
+++ Server/app/api/__init__.py
@@ -10,7 +10,7 @@ api_router.include_router(basic_health.router, prefix="/basic-health", tags=["ba
 api_router.include_router(bill.router, prefix="/bill", tags=["bill"])
 api_router.include_router(doctor_note.router, prefix="/doctor-note", tags=["doctor-note"])
 api_router.include_router(emergency_contact.router, prefix="/emergency-contact", tags=["emergency-contact"])
-api_router.include_router(notify_runner.router, prefix="notify-runner", tags=["notify-runner"])
+api_router.include_router(notify_runner.router, prefix="/notify-runner", tags=["notify-runner"])
 api_router.include_router(prescription.router, prefix="/prescription", tags=["prescription"])
 api_router.include_router(user.router, prefix="/user", tags=["user"])
 api_router.include_router(vaccination.router, prefix="/vaccination", tags=["vaccination"])
