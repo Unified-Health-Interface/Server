9a3276113668f8d9ea1b38f49bb93d392cd4abcc
Harikeshav R <r.harikeshav@gmail.com>
2023-02-01 12:25:17 +0530

    Import notify runner model to models/__init__.py

--- Server/app/models/__init__.py
+++ Server/app/models/__init__.py
@@ -4,6 +4,7 @@ from app.models.basic_health import BasicHealth
 from app.models.bill import Bill
 from app.models.doctor_note import DoctorNote
 from app.models.emergency_contact import EmergencyContact
+from app.models.notify_runner import NotifyRunner
 from app.models.prescription import Prescription
 from app.models.user import User
 from app.models.vaccination import Vaccination
