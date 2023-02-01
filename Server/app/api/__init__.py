ca4de89a0cc50469afbfa2140522eb71f6b037fc
Harikeshav R <r.harikeshav@gmail.com>
2023-02-01 12:26:51 +0530

    Import notify runner route to api/__init__.py

--- Server/app/api/__init__.py
+++ Server/app/api/__init__.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter
 
 from app.api.endpoints import allergy, appointment, user, basic_health, bill, doctor_note, emergency_contact, \
-    prescription, \
-    vaccination
+    notify_runner, prescription, vaccination
 
 api_router = APIRouter()
 api_router.include_router(allergy.router, prefix="/allergy", tags=["allergy"])
@@ -11,6 +10,7 @@ api_router.include_router(basic_health.router, prefix="/basic-health", tags=["ba
 api_router.include_router(bill.router, prefix="/bill", tags=["bill"])
 api_router.include_router(doctor_note.router, prefix="/doctor-note", tags=["doctor-note"])
 api_router.include_router(emergency_contact.router, prefix="/emergency-contact", tags=["emergency-contact"])
+api_router.include_router(notify_runner.router, prefix="notify-runner", tags=["notify-runner"])
 api_router.include_router(prescription.router, prefix="/prescription", tags=["prescription"])
 api_router.include_router(user.router, prefix="/user", tags=["user"])
 api_router.include_router(vaccination.router, prefix="/vaccination", tags=["vaccination"])
